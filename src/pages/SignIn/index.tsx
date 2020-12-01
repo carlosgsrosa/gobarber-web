42f46b234a25220dfc52c7868e0878c4e9d02777
Carlos Rosa <carlao.garcia@gmail.com>
2020-12-01 14:32:58 -0300

    Improvements: Add SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn,FiMail, FiLock } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
+
+import { Button, Input } from "../../components";
 
 import { Container, Content, Background } from "./styles";
 
@@ -12,9 +14,9 @@ const SignIn: React.FC = () => (
 
       <form>
         <h1>Faça seu Logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
